d33c4e32ee36c90cfd88e06d87f1c5a827ba0765
jaidelman <jaidelma@uoguelph.ca>
2021-10-11 22:58:24 -0400

    TeamColors

---
+++ teamColors.py
@@ -0,0 +1,54 @@
+RED = "0xFF0000"
+LIGHT_PURPLE = "0x753BBD"
+DARK_PURPLE = "0x5A1269"
+LAKERS_PURPLE = "0x552583"
+LIGHT_BLUE = "0x1D428A"
+DARK_BLUE = "0x002D62"
+NAVY_BLUE = "0x0E2240"
+MIAMI_BLUE = "0x41B6E6"
+ORANGE = "0xEF3B24"
+YELLOW = "0xFFC72C"
+GREEN = "0x007A33"
+LIME = "0xAEC120"
+WHITE = "0xFFFFFF"
+WINE = "0x6F263D"
+GOLD = "0xFFB81C"
+PINK = "0xDB3EB1"
+ORANGE = "0xE56020"
+ORANGE_RED = "0xEF3B24"
+GRAY = "0x637270"
+
+colors = {
+    "BOS": {
+        "primary": RED,
+        "alt": NAVY_BLUE
+    },
+    "TB": {
+        "primary": DARK_BLUE,
+        "alt": LIGHT_BLUE
+    },
+    "SF": {
+        "primary": ORANGE,
+        "alt": WHITE
+    },
+    "LAD": {
+        "primary": LIGHT_BLUE,
+        "alt": RED
+    },
+    "MIL": {
+        "primary": YELLOW,
+        "alt": NAVY_BLUE
+    },
+    "ATL": {
+        "primary": RED,
+        "alt": DARK_BLUE
+    }, 
+    "HOU": {
+        "primary": ORANGE,
+        "alt": DARK_BLUE
+    },
+    "CWS": {
+        "primary": NAVY_BLUE,
+        "alt": RED
+    }
+}
