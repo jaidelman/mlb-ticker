3ed56a13f578240b41d7d9cd160d3ae9ca2b26a6
Josh Aidelman <jaidelma@uoguelph.ca>
2022-05-09 15:47:53 -0400

    Add all team colours

--- teamColors.py
+++ teamColors.py
@@ -1,4 +1,5 @@
 RED = "0xFF0000"
+DARK_RED = "0xA71930"
 LIGHT_PURPLE = "0x753BBD"
 DARK_PURPLE = "0x5A1269"
 LAKERS_PURPLE = "0x552583"
@@ -17,29 +18,47 @@ PINK = "0xDB3EB1"
 ORANGE = "0xE56020"
 ORANGE_RED = "0xEF3B24"
 GRAY = "0x637270"
+SAND = "0xE3D4AD"
+SEATTLE_GREEN = "0x005C5C"
 
 colors = {
+    "ARI": {
+        "primary": DARK_RED,
+        "alt": SAND
+    },
+    "ATL": {
+        "primary": RED,
+        "alt": DARK_BLUE
+    }, 
+    "BAL": {
+        "primary": ORANGE,
+        "alt": WHITE
+    },
     "BOS": {
         "primary": RED,
         "alt": NAVY_BLUE
     },
-    "TB": {
-        "primary": DARK_BLUE,
-        "alt": LIGHT_BLUE
-    },
-    "SF": {
-        "primary": ORANGE,
-        "alt": WHITE
-    },
-    "LAD": {
-        "primary": LIGHT_BLUE,
+    "CLE": {
+        "primary": NAVY_BLUE,
         "alt": RED
     },
-    "MIL": {
-        "primary": YELLOW,
-        "alt": NAVY_BLUE
+    "CIN": {
+        "primary": RED,
+        "alt": WHITE
+    }, 
+    "CHC": {
+        "primary": DARK_BLUE,
+        "alt": RED
+    }, 
+    "COL": {
+        "primary": DARK_PURPLE,
+        "alt": WHITE
     },
-    "ATL": {
+    "CWS": {
+        "primary": NAVY_BLUE,
+        "alt": RED
+    },
+    "DET": {
         "primary": RED,
         "alt": DARK_BLUE
     }, 
@@ -47,8 +66,80 @@ colors = {
         "primary": ORANGE,
         "alt": DARK_BLUE
     },
-    "CWS": {
-        "primary": NAVY_BLUE,
+    "KC": {
+        "primary": LIGHT_BLUE,
+        "alt": SAND
+    }, 
+    "LAA": {
+        "primary": RED,
+        "alt": WHITE
+    },
+    "LAD": {
+        "primary": LIGHT_BLUE,
         "alt": RED
+    },
+    "MIN": {
+        "primary": RED,
+        "alt": DARK_BLUE
+    }, 
+    "MIL": {
+        "primary": YELLOW,
+        "alt": NAVY_BLUE
+    },
+    "MIA": {
+        "primary": MIAMI_BLUE,
+        "alt": RED
+    },
+    "NYM": {
+        "primary": ORANGE,
+        "alt": NAVY_BLUE
+    },
+    "NYY": {
+        "primary": NAVY_BLUE,
+        "alt": WHITE
+    },
+    "OAK": {
+        "primary": GREEN,
+        "alt": YELLOW
+    },
+    "PHI": {
+        "primary": RED,
+        "alt": NAVY_BLUE
+    },
+    "PIT": {
+        "primary": YELLOW,
+        "alt": DARK_RED
+    },
+    "SEA": {
+        "primary": SEATTLE_GREEN,
+        "alt": NAVY_BLUE
+    },
+    "SD": {
+        "primary": SAND,
+        "alt": NAVY_BLUE
+    },
+    "SF": {
+        "primary": ORANGE,
+        "alt": WHITE
+    },
+    "STL": {
+        "primary": RED,
+        "alt": NAVY_BLUE
+    },
+    "TB": {
+        "primary": DARK_BLUE,
+        "alt": LIGHT_BLUE
+    },
+    "TEX": {
+        "primary": LIGHT_BLUE,
+        "alt": RED
+    },
+    "TOR": {
+        "primary": DARK_BLUE,
+        "alt": RED
+    },
+    "WAS": {
+        "primary": DARK_RED,
+        "alt": NAVY_BLUE
     }
 }
